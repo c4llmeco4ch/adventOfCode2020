412444722e62aa50e5661c88ae79fbfc77aacbed
c4llmeco4ch <cbb692@gmail.com>
2020-12-03 09:29:52 -0500

    Updated Part 2

--- day3/count_collisions.py
+++ day3/count_collisions.py
@@ -6,12 +6,12 @@ with open('slope.txt') as f:
     for line in f.readlines()[1:]:
         line = line.strip()
         x = (x + 3) % len(line)
-        print(f'{line[:x]}[{line[x]}]{line[x+1:]} | {x}')
         if line[x] == '#':
             trees += 1
 print(trees)
 
 # PART 2
+
 xSpeed = [1, 3, 5, 7, 1]
 ySpeed = [1, 1, 1, 1, 2]
 x = [0] * len(xSpeed)
@@ -23,9 +23,6 @@ with open('slope.txt') as f:
         shouldNotSkip = [(pos - 1) % yS == 0 for yS in ySpeed]
         x = [(x[j] + (xS if shouldNotSkip[j] else 0)) % len(line) for j, xS in enumerate(xSpeed)]
         trees = [trees[p] + (1 if line[i] == '#' and shouldNotSkip[p] else 0) for p, i in enumerate(x)]
-total = 1
-print(trees)
-for val in trees:
-    total *= val
-print(total)
 
+from functools import reduce
+print(reduce(lambda x, y: x * y, trees))
